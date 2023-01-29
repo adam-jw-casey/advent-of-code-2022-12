1ab943533fd18d8b61eac5751ba92e8ecd0ed808
Adam Casey <adam.jw.casey@gmail.com>
2023-01-29 12:42:30 -0700

    Just need to implement the actual search algorithm

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use std::collections::HashSet;
+
+#[derive(Debug, Eq, Hash, PartialEq)]
 pub enum Move{
     Up,
     Down,
@@ -5,10 +8,10 @@ pub enum Move{
     Right
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
 pub struct Position{
-    pub x: u32,
-    pub y: u32
+    pub x: usize,
+    pub y: usize
 }
 
 #[derive(Debug, PartialEq)]
@@ -65,8 +68,8 @@ impl Surface{
             .collect::<Vec<Vec<char>>>();
 
         let best_signal = Position{
-            x: height_str.lines().filter(|l| l.contains('E')).next().unwrap().find('E').unwrap() as u32,
-            y: height_str.lines().enumerate().filter(|(_, l)| l.contains('E')).map(|(i,_)| i).next().unwrap() as u32
+            x: height_str.lines().filter(|l| l.contains('E')).next().unwrap().find('E').unwrap(),
+            y: height_str.lines().enumerate().filter(|(_, l)| l.contains('E')).map(|(i,_)| i).next().unwrap()
         };
 
         Surface{
@@ -74,11 +77,20 @@ impl Surface{
             heights
         }
     }
+
+    pub fn width(&self) -> usize{
+        self.heights[0].len()
+    }
+
+    pub fn height(&self) -> usize{
+        self.heights.len()
+    }
 }
 
 #[derive(Debug, PartialEq)]
 pub struct Player{
-    pub position: Position
+    pub position: Position,
+    pub previous: HashSet<Position>
 }
 
 impl Player{
@@ -87,13 +99,15 @@ impl Player{
     /// ```
     /// use advent_of_code_2022_12::Player;
     /// use advent_of_code_2022_12::Position;
+    /// use std::collections::HashSet;
     ///
     /// assert_eq!(
     ///     Player{
     ///         position: Position{
     ///             x: 0,
     ///             y: 0
-    ///         }
+    ///         },
+    ///         previous: HashSet::new()
     ///     },
     ///     Player::new(concat!(
     ///         "Sabqponm\n",
@@ -106,23 +120,73 @@ impl Player{
     pub fn new(map_str: &str) -> Self{
         Player{
             position: Position{
-                x: map_str.lines().filter(|l| l.contains('S')).next().unwrap().find('S').unwrap() as u32,
-                y: map_str.lines().enumerate().filter(|(_, l)| l.contains('S')).map(|(i,_)| i).next().unwrap() as u32
-            }
+                x: map_str.lines().filter(|l| l.contains('S')).next().unwrap().find('S').unwrap(),
+                y: map_str.lines().enumerate().filter(|(_, l)| l.contains('S')).map(|(i,_)| i).next().unwrap()
+            },
+            previous: HashSet::new()
         }
     }
 
     pub fn step(&mut self, dir: Move){
+        self.previous.insert(self.position);
+
         match dir{
-            Move::Up   =>  self.position.y += 1,
-            Move::Down =>  self.position.y -= 1,
+            Move::Up   =>  self.position.y -= 1,
+            Move::Down =>  self.position.y += 1,
             Move::Left =>  self.position.x -= 1,
             Move::Right => self.position.x += 1
-        }
+        };
     }
 
-    pub fn available_moves(&self, surface: &Surface) -> Vec<Move>{
-        todo!();
+    /// Returns a list of moves available to the player at its position
+    /// # Examples
+    /// ```
+    /// use advent_of_code_2022_12::Player;
+    /// use advent_of_code_2022_12::Surface;
+    /// use advent_of_code_2022_12::Move;
+    /// use std::collections::HashSet;
+    ///
+    /// let player = Player::new(concat!(
+    ///     "Sabqponm\n",
+    ///     "abcryxxl\n",
+    ///     "accszExk\n",
+    ///     "acctuvwj\n",
+    ///     "abdefghi"
+    /// ));
+    /// let surface = Surface::new(concat!(
+    ///     "Sabqponm\n",
+    ///     "abcryxxl\n",
+    ///     "accszExk\n",
+    ///     "acctuvwj\n",
+    ///     "abdefghi"
+    /// ));
+    ///
+    /// assert_eq!(
+    ///     player.available_moves(&surface),
+    ///     HashSet::from([Move::Right, Move::Down])
+    /// );
+    /// ```
+    pub fn available_moves(&self, surface: &Surface) -> HashSet<Move>{
+        let mut moves = HashSet::new();
+
+        if self.position.x < surface.width() - 1
+            && surface.heights[self.position.x][self.position.y] as u8 + 1 >= surface.heights[self.position.x+1][self.position.y] as u8{
+                moves.insert(Move::Right);
+        }
+        if self.position.x > 0
+            && surface.heights[self.position.x][self.position.y] as u8 + 1 >= surface.heights[self.position.x-1][self.position.y] as u8{
+                moves.insert(Move::Left);
+        }
+        if self.position.y < surface.height() - 1
+            && surface.heights[self.position.x][self.position.y] as u8 + 1 >= surface.heights[self.position.x][self.position.y+1] as u8{
+                moves.insert(Move::Down);
+        }
+        if self.position.y > 0
+            && surface.heights[self.position.x][self.position.y] as u8 + 1 >= surface.heights[self.position.x][self.position.y-1] as u8{
+                moves.insert(Move::Up);
+        }
+
+        return moves;
     }
 }
 
